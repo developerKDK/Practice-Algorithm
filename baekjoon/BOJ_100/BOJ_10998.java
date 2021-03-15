474bf437cc5ab12e601d7bda474dd1b463f7ef12
ehdals9412 <ehdals9412@gmail.com>
2021-03-15 18:21:35 +0900

    feat(Dongmin-Kim): [BOJ, 10998] AxB

---
+++ baekjoon/BOJ_100/BOJ_10998.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10998 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		br.close();
+		
+		while(st.hasMoreTokens()) {
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			System.out.println(A * B);
+		}
+	}
+}
